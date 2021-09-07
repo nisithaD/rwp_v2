859651d2ae2dad9a6df1c748736fdf7d8b41a3ac
Nisitha Dayarathna <nisitha56190@gmail.com>
2021-09-07 07:48:27 +0530

    git ignore added

--- application/views/templates/side_menubar.php
+++ application/views/templates/side_menubar.php
@@ -6,7 +6,7 @@
       <ul class="sidebar-menu" data-widget="tree">
         
         <li id="dashboardMainMenu">
-          <a href="<?php echo base_url('dashboard') ?>">
+          <a href="<?php echo ('/dashboard') ?>">
             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
           </a>
         </li>
