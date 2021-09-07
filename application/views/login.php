99ce379bcf5ad1c437197f61c1532acc805bb257
Nisitha Dayarathna <nisitha56190@gmail.com>
2021-09-07 10:09:08 +0530

    login dashboard changes

--- application/views/login.php
+++ application/views/login.php
@@ -1,137 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Log in</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css') ?>">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/font-awesome/css/font-awesome.min.css') ?>">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/Ionicons/css/ionicons.min.css') ?>">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/AdminLTE.min.css') ?>">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/plugins/iCheck/square/blue.css') ?>">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-  <!-- Google Font -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
+    <title>Isuru Apparel (PVT) Ltd. | Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/public/vendor/bootstrap/css/bootstrap.min.css')?>">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/public/fonts/font-awesome-4.7.0/css/font-awesome.min.css')?>">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/public/fonts/Linearicons-Free-v1.0.0/icon-font.min.css')?>">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/public/vendor/animate/animate.css')?>">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/public/vendor/css-hamburgers/hamburgers.min.css')?>">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/public/vendor/animsition/css/animsition.min.css')?>">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/public/css/util.css')?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/public/css/main.css')?>">
+    <!--===============================================================================================-->
 </head>
-<style>
-    body {
-        font-family: "Lato", sans-serif;
-    }
+<body style="background-color: #666666;">
 
-    .main-head{
-        height: 150px;
-        background: #FFF;
-
-    }
-
-    .sidenav {
-        height: 100%;
-        background: #ff0084;  /* fallback for old browsers */
-        background: -webkit-linear-gradient(to right, #022f53, #3c8dbc);  /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(to right, #022f53, #3c8dbc); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-        overflow-x: hidden;
-        padding-top: 20px;
-    }
-
-
-    .main {
-        padding: 0px 0px;
-        margin-top: -8%;
-    }
-
-    @media screen and (max-height: 450px) {
-        .sidenav {padding-top: 15px;}
-    }
-
-    @media screen and (max-width: 450px) {
-        .login-form{
-            margin-top: 10%;
-        }
-
-        .register-form{
-            margin-top: 10%;
-        }
-    }
-
-    @media screen and (min-width: 768px){
-        .main{
-            margin-left: 40%;
-        }
-
-        .sidenav{
-            width: 40%;
-            position: fixed;
-            z-index: 1;
-            top: 0;
-            left: 0;
-        }
-
-        .login-form{
-            margin-top: 80%;
-        }
-
-        .register-form{
-            margin-top: 20%;
-        }
-    }
-
-
-    .login-main-text{
-        margin-top: 20%;
-        padding: 60px;
-        color: #fff;
-    }
-
-    .login-main-text h2{
-        font-weight: 300;
-    }
-
-    .login-sub-text{
-        padding: 60px;
-        color: #fff;
-        margin-top: 25%;
-    }
-
-    .btn-black{
-        background-color: #000 !important;
-        color: #fff;
-    }
-</style>
-
-<body class="hold-transition login-page">
-<div class="sidenav">
-    <div class="login-main-text">
-        <img src=""/>
-        <h2>Isuru Apperal (pvt) Ltd.<br></h2>
-        <h3>Management Portal</h3>
-        <p>Sign in from here to access.</p>
-    </div>
-
-    <div class="login-sub-text">
-        <p>Copyrights &copy; Isuru Apparel (pvt) Ltd.</p>
-    </div>
-</div>
-<div class="main">
-    <div class="col-md-6 col-sm-12" style="margin-left: 25%;">
-        <div class="login-form">
-            <div class="login-box-body">
-                <p class="login-box-msg">Sign in to start your session</p>
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <form action="<?php echo base_url('auth/login') ?>" method="post" class="login100-form validate-form">
+                <span class="login100-form-title p-b-43">
+						Isuru Apparel (pvt) Ltd.
+					</span>
+					<span style="font-size: 25px" class="login100-form-title p-b-43">
+						Login to continue
+					</span>
 
                 <?php echo validation_errors(); ?>
 
@@ -139,51 +41,49 @@
                     echo $errors;
                 } ?>
 
-                <form action="<?php echo base_url('auth/login') ?>" method="post">
-                    <div class="form-group has-feedback">
-                        <input type="email" class="form-control" name="email" id="email" placeholder="Email" autocomplete="off">
-                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                    </div>
-                    <div class="form-group has-feedback">
-                        <input type="password" class="form-control" name="password" id="password" placeholder="Password" autocomplete="off">
-                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-8">
-                            <div class="checkbox icheck">
-                                <label>
-                                    <input type="checkbox"> Remember Me
-                                </label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-xs-4">
-                            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
+                <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                    <input type="email"  class="input100" name="email" id="email" autocomplete="off">
+                    <span class="focus-input100"></span>
+                    <span class="label-input100">Email</span>
+                </div>
 
+
+                <div class="wrap-input100 validate-input" data-validate="Password is required">
+                    <input type="password" class="input100" name="password" id="password" autocomplete="off">
+                    <span class="focus-input100"></span>
+                    <span class="label-input100">Password</span>
+                </div>
+
+                <div class="container-login100-form-btn">
+                    <button type="submit" class="login100-form-btn">
+                        Login
+                    </button>
+                </div>
+            </form>
+
+            <div class="login100-more" style="background-image: url('<?php echo base_url('assets/public/images/bg-01.jpg')?>');">
             </div>
         </div>
     </div>
 </div>
-<!-- jQuery 3 -->
 
-<script src="<?php echo base_url('assets/bower_components/jquery/dist/jquery.min.js') ?>"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="<?php echo base_url('assets/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+<!--===============================================================================================-->
+<script src="<?php echo base_url('assets/public/vendor/jquery/jquery-3.2.1.min.js')?>"></script>
+<!--===============================================================================================-->
+<script src="<?php echo base_url('assets/public/vendor/animsition/js/animsition.min.js')?>"></script>
+<!--===============================================================================================-->
+<script src="<?php echo base_url('assets/public/vendor/bootstrap/js/popper.js')?>"></script>
+<script src="<?php echo base_url('assets/public/vendor/bootstrap/js/bootstrap.min.js')?>"></script>
+<!--===============================================================================================-->
+<script src="<?php echo base_url('assets/public/vendor/select2/select2.min.js')?>"></script>
+<!--===============================================================================================-->
+<script src="<?php echo base_url('assets/public/vendor/daterangepicker/moment.min.js')?>"></script>
+<script src="<?php echo base_url('assets/public/vendor/daterangepicker/daterangepicker.js')?>"></script>
+<!--===============================================================================================-->
+<script src="<?php echo base_url('assets/public/vendor/countdowntime/countdowntime.js')?>"></script>
+<!--===============================================================================================-->
+<script src="<?php echo base_url('assets/public/js/main.js')?>"></script>
 
-<!-- iCheck -->
-<script src="<?php echo base_url('assets/plugins/iCheck/icheck.min.js') ?>"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
-    });
-  });
-</script>
+
 </body>
 </html>
